f608c248c1f1c282cf031660312ba338c0d6c148
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2024-01-12 13:08:50 +0100

    comments cleared

--- src/hooks/useManageEye.ts
+++ src/hooks/useManageEye.ts
@@ -1,9 +1,8 @@
-import { MouseEventHandler, MutableRefObject, useCallback, useRef } from "react";
+import { MutableRefObject, useCallback } from "react";
 
 export const useManageEye = (input: MutableRefObject<HTMLInputElement | null>) => {
     const toggleInputType = useCallback(
         (event: any) => {
-            console.log("clicked");
             if (input.current) {
                 event.stopPropagation();
                 if (input.current.type === "password") {
